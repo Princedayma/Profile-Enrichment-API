98a6bff5d86b4fba355117ab14cecfd51ea192e7
prince <princedayma230@gmail.com>
2025-07-18 00:39:15 +0530

    update code

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,js}"], // Include all HTML files in your public folder
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
