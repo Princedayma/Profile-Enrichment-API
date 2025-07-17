80964417b07886013964decc22ede576690693bc
prince <princedayma230@gmail.com>
2025-07-18 00:42:42 +0530

    update code

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.post('/users/enrich', async (req, res) => {
       timeout: 15000, 
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    });
+   },
+      });
     const html = response.data;
 
     const $ = cheerio.load(html);
